4bad574159204b5e632be7dbec30ba15625c85a9
patrickzhou1234 <patrickchang44543@gmail.com>
2023-02-24 10:24:47 -0800

    sixcases

---
+++ hungrycow/threecases.cpp
@@ -0,0 +1,53 @@
+// http://usaco.org/index.php?page=viewproblem&cpid=1287
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+struct bale
+{
+    long long day, amt;
+};
+
+int main()
+{
+    int n, i, j, ct;
+    long long t;
+    cin >> n >> t;
+    bool days[t] = {false};
+    bale bales[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> bales[i].day >> bales[i].amt;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = bales[i].day - 1; j < bales[i].day + bales[i].amt - 1; j++)
+        {
+            if (!days[j])
+            {
+                days[j] = true;
+            }
+            else
+            {
+                bales[i].amt++;
+            }
+            if (j == t)
+            {
+                break;
+            }
+        }
+    }
+    ct = 0;
+    for (i = 0; i < t; i++)
+    {
+        if (days[i])
+        {
+            ct++;
+        }
+    }
+    cout << ct;
+    system("pause");
+    return 0;
+}
